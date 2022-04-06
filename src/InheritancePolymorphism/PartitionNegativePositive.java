4381f2c4e4d87c71cae4a39173a0372fc10a0c5d
Rahul <prasadrahulprn3@gamil.com>
2022-04-06 22:52:51 +0530

    Partition Negative and Positive in an Array

---
+++ src/InheritancePolymorphism/PartitionNegativePositive.java
@@ -0,0 +1,54 @@
+package InheritancePolymorphism;
+import java.util.*;
+
+public class PartitionNegativePositive {
+
+
+
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            partitionNegativeAndPositive(n, arr);
+        }
+
+        // Method to partition negative and positive numbers without comparing with 0
+        static void partitionNegativeAndPositive(int n, int[] arr) {
+            ArrayList<Integer> list1 = new ArrayList<>();
+            ArrayList<Integer> list2 = new ArrayList<>();
+            // arr[0] is stored in list1
+            list1.add(arr[0]);
+            for (int i = 1; i < n; i++) {
+                // If arr[i] and arr[0] has opposite signs store arr[i] in list2
+                if ((arr[0] ^ arr[i]) < 0) {
+                    list2.add(arr[i]);
+                }
+                // If arr[i] and arr[0] has same signs store arr[i] in list1
+                else {
+                    list1.add(arr[i]);
+                }
+            }
+            // Printing elements in list1
+            for (int i = 0; i < list1.size(); i++){
+                System.out.print(list1.get(i)+" ");
+            }
+            System.out.println();
+
+            // Printing elements in list2
+            if (list2.size() > 0) {
+                for (int i = 0; i < list2.size(); i++) {
+                    System.out.print(list2.get(i) + " ");
+                }
+            } else {
+                if (arr[0] > -1) {
+                    System.out.println("Array doesn't have negative numbers");
+                } else
+                    System.out.println("Array doesn't have positive numbers");
+            }
+        }
+    }
+
+
